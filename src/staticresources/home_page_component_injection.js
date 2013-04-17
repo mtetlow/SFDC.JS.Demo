3bdf61d2a3170aa748db1aa1d3842ab72230edca
mtetlow <Mikename@gmail.com>
2013-04-16 19:22:50 -0600

    Initial commit

---
+++ src/staticresources/home_page_component_injection.js
@@ -0,0 +1,17 @@
+<br>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+<script type="text/javascript"> 
+	$j=jQuery.noConflict();
+	$j(document).ready(function(){ 
+		function listener(event){
+		  console.log(event);
+		  $j('#opp7_ileinner').html(event.data);
+		}
+
+		if (window.addEventListener){
+		  addEventListener("message", listener, false)
+		} else {
+		  attachEvent("onmessage", listener)
+		}
+	});
+</script>
